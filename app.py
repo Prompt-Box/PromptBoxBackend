8ce8b9e6504662c01c517a24042f0268dd391afa
Max <m_medearis@ucsb.edu>
2020-10-18 19:44:06 -0700

    added routes

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ from flask import Flask, render_template
 from transformers import pipeline
 app = Flask(__name__)
 
-generator = pipeline('text-generation', model='gpt2')
-
-out = generator("Hello World", max_length=30, num_return_sequences=1)
 
 
 @app.route("/")
 def hello():
+    return "Hello World"
+
+@app.route('/text/<string:input>')
+def generate_text(input):
+    generator = pipeline('text-generation', model='gpt2')
+    out = generator(input, max_length=30, num_return_sequences=1)
+
     return str(out)
+
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port = 4000)
