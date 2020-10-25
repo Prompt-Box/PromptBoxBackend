9413f0cbb20dda8a4e1d76067a77d9889c6ee092
Max <m_medearis@ucsb.edu>
2020-10-25 15:35:21 -0700

    added join lobby, create lobby, create game, get lobby, get game status, edit game values

--- app.py
+++ app.py
@@ -1,36 +1,44 @@
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, Response
 app = Flask(__name__)
 
 #sample JSON formatted Game, to be replaced with SQL database
-lobbies = [
+# lobby format: id: int, title: str, players: list(str), num_players: int
+
+lobby = [
     {
         'id': 1,
         'title': u'Prompt Box Party',
-        'description': u'This game is not full, please join!',
-        'num_players': 0,
+        'players': [u'Max'],
+        'num_players': 1,
     },
 
     {
         'id': 2,
         'title': u'Game is Full',
-        'description': u'This game is already full. Sorry = (',
+        'players': [u'Shay', u'Benny'],
         'num_players': 2
     }
 ]
 
-active_games = [
+# game format, id: int, title: str, players: list(str), player1_turn: True, score: list(int)
+# round: 1
+games = [
     {
         'id': 1,
-        'players': [u'Max', u'Akhil'],
+        'players': {'Max': 0, 'Akhil': 0},
         'player1_turn': True,
-        'score': [0, 0],
         'round': 1
 
     }
 ]
 
+
+
+
 # Placeholder Function to create AI-Generated Text
+"""Test Functions"""
+
 def get_text(user_input):
     str = """Lorem ipsum dolor sit amet, consectetur adipiscing elit,
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -47,13 +55,7 @@ def hello():
 def generate_text(input):
     return jsonify({'generated_text': get_text(input)})
 
-# Return All Active Lobbies
-@app.route('/api/games', methods=["GET"])
-def get_games():
-    return jsonify({'games': lobbies})
-
-# Return All Open Lobbies
-@app.route('/api/games/open', methods=['GET'])
+"""@app.route('/api/games/open', methods=['GET'])
 def get_task():
     open_games = [game for game in lobbies if game['num_players'] < 2]
     return jsonify({'games': open_games})
@@ -61,10 +63,107 @@ def get_task():
 @app.route('/api/games/active/<int: game_id>', methods=['GET'])
 def get_games_by_id():
     game = [game for game in active_games if game['id']== game_id]
-    return jsonify({'game': game})
+    return jsonify({'game': game})"""
+
+"""Test Functions"""
+
+# Return All Games that have not started
+@app.route('/api/lobby', methods=["GET"])
+def get_lobby():
+    return jsonify({'lobby': lobby})
+
+# Let a Player Join a Lobby
+@app.route('/api/lobby/join/<int:id>/<string:name>', methods=["POST"])
+def join_game(id, name):
+    for l in lobby:
+        if l['id'] == id:
+            l['players'].append(name)
+            l['num_players'] += 1
+            # If the lobby is full, create a new game
+            if l['num_players'] == 2:
+                games.append({
+                        'id': len(games) + 1,
+                        'players': dict.fromkeys(l['players'], 0),
+                        'player1_turn': True,
+                        'round': 1
+                })
+                #
+                return jsonify({'game':games[len(games)-1]}), 200
+
+    # Passed Invalid Lobby ID
+    return Response(
+        "Lobby ID Not valid",
+        status=400,
+    )
+
+# Let a Player Create a Lobby
+@app.route('/api/lobby/create/<string:title>/<string:name>', methods=["POST"])
+def create_game(title, name):
+    lobby.append({
+        'id': len(lobby)+1,
+        'title': title,
+        'players': [name],
+        'num_players': 1
+    })
+    return "True"
+
+
+# Return Game status
+@app.route('/api/games/<int:id>', methods=['POST'])
+def get_game(id):
+    for game in games:
+        if game['id'] == id:
+            return jsonify({'games': game})
+
+    return Response(
+        "Game ID Not valid",
+        status=400,
+    )
+
+"""Methods to update a game"""
+
+# Update Score
+@app.route('/api/games/<int:id>/<string:name>', methods=['POST'])
+def update_score(id, name):
+    for game in games:
+        if game["id"] == id:
+            game["players"][name] += 1
+            return "Score Edited"
+    return Response(
+        "Game ID Not valid",
+        status=400,
+    )
+
+# Update Turn
+@app.route('/api/games/<int:id>/turn', methods=['POST'])
+def update_turn(id):
+    for game in games:
+        if game["id"] == id:
+            game["player1_turn"] = not game["player1_turn"]
+            return "Turn Updated"
+    return Response(
+        "Game ID Not valid",
+        status=400,
+    )
+
+# Update Round
+@app.route('/api/games/<int:id>/round', methods=['POST'])
+def update_round(id):
+    for game in games:
+        if game["id"] == id:
+            game["round"] += 1
+            return "Round Updated"
+    return Response(
+        "Game ID Not valid",
+        status=400,
+    )
+
+
+
 
 
 
 if __name__ == '__main__':
+
     # threaded, so many users can use
     app.run(threaded=True, port=4000)
