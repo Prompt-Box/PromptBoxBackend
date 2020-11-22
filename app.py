5c07099dbb0e565186236d325793afc7b1e63910
Samuel Guo <samuelzguo@gmail.com>
2020-11-21 17:42:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -106,10 +106,10 @@ def get_lobby():
     lobbies = [serialize_lobby(lobby) for lobby in Lobby.query.all()]
     db.session.close()
     response = jsonify({"lobbies" : lobbies})
-    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:5000/')
-    response.headers.add('Access-Control-Allow-Credentials', True)
-    response.headers.add('Access-Control-Allow-Methods' : 'GET')
-    response.headers.add('Access-Control-Allow-Headers' : 'Origin', 'Content-Type', 'Accept')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Credentials', True)
+    # response.headers.add('Access-Control-Allow-Methods' : 'GET')
+    # response.headers.add('Access-Control-Allow-Headers' : 'Origin', 'Content-Type', 'Accept')
     return response
 
 # Let a Player Join a Lobby
